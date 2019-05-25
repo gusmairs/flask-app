1a9d20373e411bd25ec1489628796cbafc2aa96b
gusmairs <gusmairs@mac.com>
2019-05-25 13:12:00 -0700

    html-pandas method working

--- app.py
+++ app.py
@@ -10,10 +10,13 @@ app = Flask(__name__, static_url_path='')
 
 contacts = pd.read_csv('names.txt')
 records = contacts.to_dict(orient='records')
+t = contacts.to_html(border=0, index=False, escape=True)
+t.split('\n')
 records
 
 @app.route('/')
 def index():
     return render_template('index.html',
                            message='Hello, my friend!',
+                           table=t,
                            records=records)
