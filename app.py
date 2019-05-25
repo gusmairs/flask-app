effd7009d774cf86dbd32a3575e2141bc8614d11
Gus <gusmairs@mac.com>
2019-05-25 14:00:25 -0700

    some css polish

--- app.py
+++ app.py
@@ -1,22 +1,15 @@
-# import os
 from flask import Flask, render_template
 import pandas as pd
 
 app = Flask(__name__, static_url_path='')
 
-# contacts = []
-# with open('names.txt', 'r') as names:
-#     contacts = names.read().splitlines()
-
 contacts = pd.read_csv('names.txt')
 records = contacts.to_dict(orient='records')
 t = contacts.to_html(border=0, index=False, escape=True)
 t.split('\n')
-records
 
 @app.route('/')
 def index():
     return render_template('index.html',
                            message='Hello, my friend!',
-                           table=t,
-                           records=records)
+                           table=t)
