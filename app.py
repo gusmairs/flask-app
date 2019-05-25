d1dca5ae7adddf467df9a7246c2b2b817c4bbbc7
gusmairs <gusmairs@mac.com>
2019-05-25 09:33:41 -0700

    basic flask/dict table with templates working

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 # import os
 from flask import Flask, render_template
+import pandas as pd
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='')
 
-contacts = []
-with open('names.txt', 'r') as names:
-    contacts = names.read().splitlines()
-print(contacts)
+# contacts = []
+# with open('names.txt', 'r') as names:
+#     contacts = names.read().splitlines()
+
+contacts = pd.read_csv('names.txt')
+records = contacts.to_dict(orient='records')
+records
 
 @app.route('/')
 def index():
     return render_template('index.html',
                            message='Hello, my friend!',
-                           contacts=contacts)
+                           records=records)
