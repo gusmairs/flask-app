85b08d604b42db5ecf2dcad25bff755de0653226
Gus <gusmairs@mac.com>
2019-05-25 14:18:24 -0700

    data import inside flask now - updates with new

--- app.py
+++ app.py
@@ -3,13 +3,10 @@ import pandas as pd
 
 app = Flask(__name__, static_url_path='')
 
-contacts = pd.read_csv('names.txt')
-records = contacts.to_dict(orient='records')
-t = contacts.to_html(border=0, index=False, escape=True)
-t.split('\n')
-
 @app.route('/')
 def index():
+    contacts = pd.read_csv('names.txt')
+    t = contacts.to_html(border=0, index=False, escape=True)
     return render_template('index.html',
                            message='Hello, my friend!',
                            table=t)
