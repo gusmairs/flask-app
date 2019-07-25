60b33c798c2a9319f8808177a0c40aa858c9a046
gusmairs <gusmairs@mac.com>
2019-07-24 18:13:34 -0700

    dynamic table display and insertion working

--- app.py
+++ app.py
@@ -1,17 +1,28 @@
 from flask import Flask, request, render_template, jsonify
 from bson.objectid import ObjectId
 import pandas as pd
-import pymongo
+import sqlite3 as sql
+# import pymongo
+
 
 app = Flask(__name__, static_url_path='')
 
-mongo = pymongo.MongoClient()
-db = mongo['one_db']
+# mongo = pymongo.MongoClient()
+# db = mongo['one_db']
+db = 'flask.db'
 
 def get_table():
-    mg_dat = db.people.find().sort('age', -1)
-    names_df = pd.DataFrame(list(mg_dat))
-    tbl = names_df[['_id', 'name', 'age']].to_html(border=0, index=False)
+    con = sql.connect(db)
+    c = con.cursor()
+    dat = c.execute('''
+        select name, age from names
+        ;
+        ''')
+    # mg_dat = db.people.find().sort('age', -1)
+    names_df = pd.DataFrame(dat.fetchall(), columns=['name', 'age'])
+    # names_df = pd.DataFrame(list(mg_dat))
+    tbl = names_df[['name', 'age']].to_html(border=0, index=False)
+    con.close()
     return tbl
 
 @app.route('/')
@@ -24,7 +35,16 @@ def show_table():
 
 @app.route('/add', methods=['POST'])
 def add_data():
-    db.people.insert_one(request.json)
+    print(request.json['name'], request.json['age'])
+    con = sql.connect(db)
+    c = con.cursor()
+    c.execute('''
+        insert into names values (?, ?)
+        ;
+        ''', (request.json['name'], request.json['age']))
+    con.commit()
+    con.close()
+    # db.people.insert_one(request.json)
     return jsonify({'table': get_table()})
 
 @app.route('/detail', methods=['POST'])
