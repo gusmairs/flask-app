52f690cf7cc7c0247d7bc8b9b0f1a944d43cd35e
gusmairs <gusmairs@mac.com>
2019-08-18 14:23:41 -0700

    refined app.py, comments added, tested

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
-from flask import Flask, request, render_template, jsonify
+#!/usr/bin/env python
+
+# Flask app here drives the web site using a MongoDB database that runs on a
+# local server port. Note how simple the querying is through pymongo, with many
+# lines of SQL query code eliminated and no parameter substitution into string
+# queries needed.
+
+# The app renders the HTML table here on the server side using the convenience
+# of the pandas::df.to_html() method. To do: leave the data in .json format and
+# send to the client for JavaScript to render into the table.
+
+from flask import (
+    Flask, request, render_template, jsonify
+)
 from bson.objectid import ObjectId
 import pandas as pd
 import pymongo
@@ -11,7 +24,10 @@ db = mongo['one_db']
 def get_table():
     mg_dat = db.people.find().sort('age', -1)
     names_df = pd.DataFrame(list(mg_dat))
-    tbl = names_df[['_id', 'name', 'age']].to_html(border=0, index=False)
+    tbl = names_df.loc[
+        :,
+        ['_id', 'name', 'age']
+    ].to_html(border=0, index=False)
     return tbl
 
 @app.route('/')
